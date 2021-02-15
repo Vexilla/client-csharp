a7d47b2a4516cefe01bbce53cfe0c795fc3e6953
liam_ <liam_@DESKTOP-R9N97O1>
2021-02-15 00:49:43 +0000

    test: Failing test for deserialization

---
+++ Vexilla.Client.Tests/JsonDeserializationTests.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Xunit;
+
+namespace Vexilla.Client.Tests
+{
+    public class JsonDeserializationTests
+    {
+        private string exampleConfig = @"{""environments"":{""test"":{""untagged"":{""db1"":{""value"":false,""type"":""toggle""}}}}}";
+
+        [Fact]
+        public void systemTextProducesSameAsNewtonsoft()
+        {
+            VexillaConfig newtonsoftConfig = JsonConvert.DeserializeObject<VexillaConfig>(exampleConfig);
+
+            Assert.IsType<VexillaConfig>(newtonsoftConfig.ToString());
+        }
+    }
+}
